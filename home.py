ddf92769f617090019ceac2d268bff4368803c55
Apoorva <apoorvaasthana48@gmail.com>
2025-01-22 16:05:34 +0530

    ..

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("TEXT - TO - SPEECH CONVERTER")
+st.markdown('''Welcome to Text to Speech Converter.Text to speech is the use of software to create a sound output in the form of a spoken 
+voice.An input text is transformed into phonetic writing.This says how the words are pronounced,aand not how they are written
+.This also converts into speech if user uploads any file or document. It also reads text aloud almost instantly for
+real time apps.Easily accessible to specially abled persons.Ideal for accessibility,content creation,and language learning
+,this app makes it easy to bring written words to life.''')
+st.image('Assets\OIP.jpeg', use_column_width=True)
